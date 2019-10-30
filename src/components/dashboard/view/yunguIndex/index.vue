d3384d28354d3555d7543f806055ee4cd99cdb8d
henglong <907161303@qq.com>
2019-10-30 17:54:04 +0800

    配置跨域

--- src/components/dashboard/view/yunguIndex/index.vue
+++ src/components/dashboard/view/yunguIndex/index.vue
@@ -1,96 +1,138 @@
 <template>
   <div class="yunguIndex">
-    <p style="color:#333">我是云股傳媒</p>
-    <div class="chart" id="chart"></div>
+    <div class="chartOne" id="chartOne">
+
+    </div>
+    <div class="chartTwo" id="chartTwo">
+ 
+    </div>
   </div>
 </template>
 <script>
 import echarts from "echarts";
+import qs from "qs";
 export default {
   name: "yunguIndex",
   methods: {
     showEcharts() {
-  this.chartLine = echarts.init(document.getElementById('chart'));
- 
-        // 指定图表的配置项和数据
-        var option = {
-            tooltip: {              //设置tip提示
-                trigger: 'axis'
-            },
-            
-            legend: {               //设置区分（哪条线属于什么）
-                data:['平均成绩','学生成绩']
-            },
-            color: ['#8AE09F', '#FA6F53'],       //设置区分（每条线是什么颜色，和 legend 一一对应）
-            xAxis: {                //设置x轴
-                type: 'category',
-                boundaryGap: false,     //坐标轴两边不留白
-                data: ['2019-1-1', '2019-2-1', '2019-3-1', '2019-4-1', '2019-5-1', '2019-6-1', '2019-7-1',],
-                name: 'DATE',           //X轴 name
-                nameTextStyle: {        //坐标轴名称的文字样式
-                    color: '#FA6F53',
-                    fontSize: 16,
-                    padding: [0, 0, 0, 20]
-                },
-                axisLine: {             //坐标轴轴线相关设置。
-                    lineStyle: {
-                        color: '#FA6F53',
-                    }
-                }
-            },
-            yAxis: {
-                name: 'SALES VOLUME',
-                nameTextStyle: {
-                    color: '#FA6F53',
-                    fontSize: 16,
-                    padding: [0, 0, 10, 0]
-                },
-                axisLine: {
-                    lineStyle: {
-                        color: '#FA6F53',
-                    }
-                },
-                type: 'value'
-            },
-            series: [
-            //   {
-            //     name: '平均成绩',
-            //     data:  [220, 232, 201, 234, 290, 230, 220],
-            //     type: 'line',               // 类型为折线图
-            //     lineStyle: {                // 线条样式 => 必须使用normal属性
-            //         normal: {
-            //             color: '#8AE09F',
-            //         }
-            //     },
-            //   },
-              {
-                name: '流程趋势',
-                data: [120, 200, 150, 80, 70, 110, 130],
-                type: 'line',
-                lineStyle: {
-                    normal: {
-                        color: 'yellow',
-                    }
-                },
+      this.chartLineOne = echarts.init(document.getElementById("chartOne"));
+      this.chartLineTwo = echarts.init(document.getElementById("chartTwo"));
+      var optionOne = {
+        title: {
+          text: "我处理的流程趋势12个月",
+          subtext: "",
+          x: "center",
+          align: "center"
+        },
+        xAxis: {
+          type: "category",
+          data: [" ", " ", " ", " ", " ", " ", " "]
+        },
+        yAxis: {
+          type: "value"
+        },
+        series: [
+          {
+            name: "我处理的流程趋势12个月",
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: "line",
+            lineStyle: {
+              normal: {
+                color: "#999"
               }
-          ]
+            }
+          }
+        ]
+      };
+      var optionTwo = {
+          title: {
+          text: "我启动的流程趋势12个月",
+          subtext: "",
+          x: "center",
+          align: "center"
+        },
+        xAxis: {
+          type: "category",
+          data: [" ", " ", " ", " ", " ", " ", " "]
+        },
+        yAxis: {
+          type: "value"
+        },
+        series: [
+          {
+            name: "我启动的流程趋势12个月",
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: "line",
+            lineStyle: {
+              normal: {
+                color: "#999"
+              }
+            }
+          }
+        ]
+      };
+      // 使用刚指定的配置项和数据显示图表。
+      this.chartLineOne.setOption(optionOne);
+      this.chartLineTwo.setOption(optionTwo);
+    },
+    getLogin(){
+        const param = {
+            userName:"test",
+            password:"123456"
         };
- 
-        // 使用刚指定的配置项和数据显示图表。
-        this.chartLine.setOption(option);
+        this.$http({
+          method:"post",
+          url:"/api/login",
+          data:qs.stringify(param),
+          headers:{
+              "Content-Type":"application/x-www-form-urlencoded"
+          }
+      }).then(res =>{
+          console.log(res.data);
+      })
     }
   },
+  created(){
+      this.getLogin();
+  },
   mounted() {
-      this.showEcharts();
+    this.showEcharts();
   }
 };
 </script>
 <style lang="scss" scoped>
 .yunguIndex {
+  width: 100%;
   padding: 30px;
-  .chart {
-    width: 600px;
-    height: 500px;
+  background-color: #ccc;
+  .chartOne,
+  .chartTwo {
+    float: left;
+    width: 48%;
+    min-width:500px;
+    height: 400px;
+    background-color: #fff;
+    border-radius:10px;
+    padding:20px;
+    box-sizing:border-box;
+    margin:10px;
+    position:relative;
+  }
+  .chartOne:after{
+      content:'已处理的文档';
+      position:absolute;
+      bottom:10px;
+      left:0;
+      text-align:center;
+      width: 100%;
+  }
+  .chartTwo:after{
+      content:'启动文档';
+      position:absolute;
+      bottom:10px;
+      left:0;
+      text-align:center;
+      width: 100%;
   }
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  lintOnSave: false
+    devServer: {
+        // 设置主机地址
+        host: 'localhost',
+        // 设置默认端口
+        port: 8080,
+        // 设置代理
+        proxy: {
+            '/api': {
+                // 目标 API 地址
+                target: 'http://192.168.1.168:8080',
+                // 如果要代理 websockets
+                ws: true,
+                // 将主机标头的原点更改为目标URL
+                changeOrigin: false,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    }
 }
